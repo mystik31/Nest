db34d32c4b60b1f7123126156525b8973ebccf39
Clément <clement.becamel@epitech.eu>
2019-01-26 12:07:50 +0100

    nest

---
+++ Nest/Assets/Script/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(1, 20)]
+    public float jumpVelocity, leftVelocity;
+
+
+    [SerializeField] private GameObject _GroundCheck;
+    [SerializeField] private LayerMask _Layer;
+    [SerializeField] private float _RadiusCircle;
+
+    void Start()
+    {
+        _motor = GetComponent<PlayerMotor>();
+    }
+  
+    void Update()
+    {
+        bool Etat = Physics2D.OverlapCircle(_GroundCheck.transform.position, _RadiusCircle, _Layer);
+        float x = Input.GetAxis("Horizontal");
+
+        _motor.RunAndJump(x);
+        if (Etat && Input.GetButtonDown("Jump"))
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+    }
+
+    private PlayerMotor _motor;
+}
---
+++ Nest/Assets/Script/PlayerMotor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(PlayerController))]
+
+public class PlayerMotor : MonoBehaviour
+{
+
+    void Start()
+    {
+        _velocity = Vector2.zero;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        PerformRunAndJump();
+    }
+
+    public void RunAndJump(float x)
+    {
+        _rb.velocity = new Vector2(x * _MaxSpeed * Time.deltaTime, _rb.velocity.y);
+    }
+
+    private void PerformRunAndJump()
+    {
+        if (_rb.velocity.y < 0)
+            _rb.velocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.deltaTime;
+        else if (_rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            _rb.velocity += Vector2.up * Physics2D.gravity.y * (5f - 1) * Time.deltaTime;
+    }
+
+    private Vector2 _velocity;
+    private Rigidbody2D _rb;
+    [SerializeField] private float _MaxSpeed;
+    [SerializeField] private float _MaxSpeedJump;
+
+}
